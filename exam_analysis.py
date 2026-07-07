6e02a54b4d3f3096372839b3efe5c5dc26193486
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-07 19:43:34 +0530

    logistic regression classification model

--- exam_analysis.py
+++ exam_analysis.py
@@ -1,18 +1,18 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-
+from sklearn.metrics import accuracy_score
 df = pd.read_csv("exam_scores.csv")
+df["pass"] = df["score"] >= 70
 
 X = df[["hours_studied", "age"]]   
-y = df["score"]
+y = df["pass"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LinearRegression()
+model = LogisticRegression()
 model.fit(X_train, y_train)
-
 predictions = model.predict(X_test)
-print("Slope for each feature:", model.coef_)
-print("Intercept:", model.intercept_)
-error = mean_absolute_error(y_test, predictions)
-print("Average error:", error)
+acc = accuracy_score(y_test, predictions)
+print("Accuracy:", acc)
+print("Actual:", y_test.values)
+print("Predicted:", predictions)
