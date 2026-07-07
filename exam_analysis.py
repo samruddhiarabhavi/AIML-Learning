8b690a5df42f6e91d9b08c3ae99151d1324caa06
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-07 19:17:06 +0530

    completed regression

--- exam_analysis.py
+++ exam_analysis.py
@@ -5,15 +5,14 @@ from sklearn.metrics import mean_absolute_error
 
 df = pd.read_csv("exam_scores.csv")
 
-X = df[["hours_studied"]]   
+X = df[["hours_studied", "age"]]   
 y = df["score"]
-x_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LinearRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
-print("Actual scores:", y_test.values)
-print("Predicted scores:", predictions)
-
+print("Slope for each feature:", model.coef_)
+print("Intercept:", model.intercept_)
 error = mean_absolute_error(y_test, predictions)
 print("Average error:", error)
