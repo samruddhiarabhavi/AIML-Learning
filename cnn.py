723ebfe65e081bde6d857a06816801e7dfa1edf7
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-10 22:31:42 +0530

    cnn

--- cnn.py
+++ cnn.py
@@ -17,6 +17,24 @@ print("Test samples:", len(test_data))
 image, label = train_data[0]
 print("Image shape:", image.shape)
 print("Label:", label)
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size = 3,padding = 1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 7 * 7, 64)
+        self.fc2 = nn.Linear(64, 10)
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+Model = CNN()
+print(Model)
 
 
 
