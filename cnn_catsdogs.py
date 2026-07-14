c6654d32dc526853d99e6ff852314282b0dddcf8
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-15 01:35:18 +0530

    train test split

--- cnn_catsdogs.py
+++ cnn_catsdogs.py
@@ -16,3 +16,15 @@ print("Classes:", dataset.classes)
 image, label = dataset[0]
 print("Image shape:", image.shape)
 print("Label:", label)
+from torch.utils.data import random_split
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+
+train_data, test_data = random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+print("Train size:", len(train_data))
+print("Test size:", len(test_data))
