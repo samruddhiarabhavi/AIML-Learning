2396e5c58dc851d635a47e14859af13207459551
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-15 20:04:18 +0530

    cnn model

--- cnn_catsdogs.py
+++ cnn_catsdogs.py
@@ -28,3 +28,22 @@ test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
 print("Train size:", len(train_data))
 print("Test size:", len(test_data))
+class CatDogCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size= 3, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32*16*16,64)
+        self.fc2 = nn.Linear(64,2)
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = CatDogCNN()
+print(model)
