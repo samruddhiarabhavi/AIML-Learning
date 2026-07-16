4f644848acef471be0fe9dcbfb25dd2b12efbfe5
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-07-16 18:56:30 +0530

    ResNet18 transfer learning

---
+++ transfer_learning.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader, random_split
+ 
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+dataset = datasets.ImageFolder(root=r"C:\Users\HP\aimllearn\training_data", transform=transform)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_data, test_data = random_split(dataset, [train_size, test_size])
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+model = models.resnet18(weights="IMAGENET1K_V1")
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.fc = nn.Linear(model.fc.in_features, 2)
+
+print(model.fc)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.fc.parameters(), lr=0.001)
+
+epochs = 5
+for epoch in range(epochs):
+    total_loss = 0
+    for images, labels in train_loader:
+        predictions = model(images)
+        loss = criterion(predictions, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    print(f"Epoch {epoch+1}, Loss: {total_loss / len(train_loader):.4f}")
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = correct / total
+print(f"Test Accuracy: {accuracy * 100:.2f}%")
